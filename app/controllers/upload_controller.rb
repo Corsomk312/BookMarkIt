bc6936dbffaab5881a3173c3bf8ac8f51950de01
John Doe <johndoe@example.com>
2016-01-31 09:40:15 -0600

    Fix bug: naming conventions in parse file call causes site to break

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -6,7 +6,7 @@ class UploadController < ApplicationController
 
   def create
     @bookmark_html = params[:upload][:upload_file_name].read
-    ParseUploadedBookmarks.call(@bookmark_html, current_user)
+    ParseUploadedBookmarksFile.call(@bookmark_html, current_user)
     redirect_to user_path(current_user)
   end
 
