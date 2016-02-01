363bf4758927ba1a44cd5725e69284c48034a823
John Doe <johndoe@example.com>
2016-02-01 09:59:11 -0600

    Add feature: user may copy another user's lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,13 +2,25 @@ class ListsController < ApplicationController
 
   def create
     @user = current_user
-    @list = @user.lists.new(list_params)
-    if @list.save
-      flash[:notice] = "List successfully created!"
-      redirect_to @user
+    if params[:list][:list_to_dup]
+      old_list = List.find(params[:list][:list_to_dup].to_i)
+      @list = old_list.dup
+      @list.update_attributes(creator_id: @user.id)
+      old_list.saved_bookmarks.to_a.each do |saved_bookmark|
+        new_saved_bookmark = saved_bookmark.dup
+        new_saved_bookmark.update_attributes(list_id: @list.id)
+      end
+      flash[:notice] = "Successfully added list!"
+      redirect_to :back
     else
-      flash[:alert] = @list.errors.full_messages.first
-      redirect_to :new_list
+      @list = @user.lists.new(list_params)
+      if @list.save
+        flash[:notice] = "List successfully created!"
+        redirect_to @user
+      else
+        flash[:alert] = @list.errors.full_messages.first
+        redirect_to :new_list
+      end
     end
   end
 
@@ -49,7 +61,7 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :list_to_dup)
   end
 
 end
