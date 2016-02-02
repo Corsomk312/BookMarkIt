d6ef77f19a8e615175d8dbfeab4be914afb22d79
Ronny Ewanek <email": null>
2016-02-02 10:58:14 -0600

    add updated test for registration

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -13,16 +13,16 @@ feature 'registration page' do
   scenario 'can register as a user' do
     visit '/users/new'
     within('#create-user') do
-      fill_in "username", with: "ronny"
-      fill_in "email", with: "ronny@gmail.com"
+      fill_in "username", with: "test"
+      fill_in "email", with: "test@gmail.com"
       fill_in "password", with: "password"
     end
     click_button "Create"
-    expect(page).to have_content "Successfully registered!"
+    expect(page).to have_content "Successfully registered and logged in!"
 
   end
 end
-exit
+
 feature 'login' do
   # before :each do
   #   User.create(:username => "ronny", :email =>"ronny@gmail.com", :password => "password")
