6b16b909ce240e46afb921c00a48a305740aec61
John Doe <johndoe@example.com>
2016-02-03 11:22:13 -0600

    Add basic multithreading, still not optimized

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'screencap'
 gem 'sidekiq'
-gem 'redis'
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- app/services/get_thumbnails.rb
+++ app/services/get_thumbnails.rb
@@ -1,6 +1,7 @@
 class GetThumbnails
 
   def self.call(thumblist)
+    puts "RECEIVED TASK FROM WORKER"
     thumblist.each do |id|
       save_snapshot_to_s3(Bookmark.find(id))
     end
@@ -9,7 +10,6 @@ class GetThumbnails
   def self.save_snapshot_to_s3(new_bookmark)
     file = File.open(AddSnapshot.call(new_bookmark.url))
     new_snapshot = Snapshot.new(bookmark_id: new_bookmark.id)
-    # or: pass snap args there! ^^^^
     new_snapshot.thumbnail = file # snapshot.save
     file.close
     new_snapshot.save! # snapshot.save
@@ -18,7 +18,3 @@ class GetThumbnails
 
 
 end
-
-#########################################################################
-  #
-##########################################################################
