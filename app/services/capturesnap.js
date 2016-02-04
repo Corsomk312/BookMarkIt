e54ab925deb2e57b551a27cb272e7965be11aef7
Lars Johnson <keepitlj@gmail.com>
2016-02-03 18:10:20 -0600

    Add reduced thumbnail capture size

--- app/services/capturesnap.js
+++ app/services/capturesnap.js
@@ -11,7 +11,7 @@ Screenshot.prototype.grab = function() {
   var page = require('webpage').create();
   page.viewportSize = { width: 1024, height: 768 };
   // page.zoomFactor = 0.25;
-  page.clipRect = { top: 0, left: 0, width: 576, height: 432 };
+  page.clipRect = { top: 0, left: 0, width: 384, height: 288 };   // Previously 576 x 432
   page.open(this.url, function() {
     page.render(filename, {format: 'jpg', quality: '20'});
     phantom.exit();
