b12e82f2a7e842f9f2c3e910b36cd32ac97004a5
Lars Johnson <keepitlj@gmail.com>
2016-02-08 06:36:12 -0600

    Add updated Readme.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 User.create(first_name: "lars", last_name: "johnson", username: "lars", email: "lars@lars", password: "lars")
 User.create(first_name: "jacob", last_name: "crofts", username: "jacob", email: "jacob@jacob", password: "jacob")
-User.create(first_name: "ronny", last_name: "Ewanek", username: "ronny", email: "ronny@ronny", password: "ronny")
+User.create(first_name: "ronny", last_name: "ewanek", username: "ronny", email: "ronny@ronny", password: "ronny")
 User.create(first_name: "kevin", last_name: "corso", username: "kevin", email: "kevin@kevin", password: "kevin")
-User.create(first_name: "sue", last_name: "sueson", username: "sue", email: "sue@sue", password: "sue")
-User.create(first_name: "jill", last_name: "jilsen", username: "jill", email: "jill@jill", password: "jill")
